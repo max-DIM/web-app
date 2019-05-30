ea5eb01c61078fe5e122f9e1da3ed77e9157b881
Maxime Girard <mx.girard42@gmail.com>
2019-05-30 13:48:09 +0200

    add first vue

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      title: 'titre'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
