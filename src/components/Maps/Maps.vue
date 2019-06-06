31f93d019bb7535822f8ac440b6544b378179eb8
Maxime Girard <mx.girard42@gmail.com>
2019-06-06 18:50:02 +0200

    put in order Maps.vue

--- src/components/Maps/Maps.vue
+++ src/components/Maps/Maps.vue
@@ -1,12 +1,53 @@
 <template>
-    <div>
-      <p>vue maps</p>
-    </div>
+  <div>vue maps</div>
 </template>
 
 <script>
 export default {
-  name: 'Maps'
+  name: 'Maps',
+  components: {
+  },
+  props: {
+  },
+  watch: {
+  },
+  computed: {
+  },
+  replace: {
+  },
+  data: function () {
+    return {
+    }
+  },
+  methods: {
+  },
+  beforeCreate () {
+    console.log('beforeCreate')
+  },
+  created () {
+    console.log('created')
+  },
+  compile () {
+    console.log('compile')
+  },
+  beforeMount () {
+    console.log('beforeMount')
+  },
+  mounted () {
+    console.log('mounted')
+  },
+  beforeUpdate () {
+    console.log('beforeUpdate')
+  },
+  updated () {
+    console.log('updated')
+  },
+  beforeDestroy () {
+    console.log('beforeDestroy')
+  },
+  destroyed () {
+    console.log('destroyed')
+  }
 }
 </script>
 
