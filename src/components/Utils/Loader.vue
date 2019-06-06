19bac4fbcc5e81a2bf2d56112b6647fb65fd3d47
Maxime Girard <mx.girard42@gmail.com>
2019-06-06 18:56:11 +0200

    put in order Loader.vue

--- src/components/Utils/Loader.vue
+++ src/components/Utils/Loader.vue
@@ -12,7 +12,50 @@
 
 <script>
 export default {
-  name: 'loader'
+  name: 'Loader',
+  components: {
+  },
+  props: {
+  },
+  watch: {
+  },
+  computed: {
+  },
+  replace: {
+  },
+  data: function () {
+    return {
+    }
+  },
+  methods: {
+  },
+  beforeCreate () {
+    console.log('beforeCreate')
+  },
+  created () {
+    console.log('created')
+  },
+  compile () {
+    console.log('compile')
+  },
+  beforeMount () {
+    console.log('beforeMount')
+  },
+  mounted () {
+    console.log('mounted')
+  },
+  beforeUpdate () {
+    console.log('beforeUpdate')
+  },
+  updated () {
+    console.log('updated')
+  },
+  beforeDestroy () {
+    console.log('beforeDestroy')
+  },
+  destroyed () {
+    console.log('destroyed')
+  }
 }
 </script>
 
